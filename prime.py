8373ea4bcef67d5b1658ea8d96cecfe305d83a4f
Praveen <37235355+praveenhp07@users.noreply.github.com>
2019-02-05 14:56:08 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,12 @@
 st1, st2 = raw_input().split()
 a=int(st1)
 b=int(st2)
+x=0
 for num in range(a,b + 1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print(num)
+           x=x+1
+print(x)  
