83d2d182605fca456eeedc408298079aa5aa2447
Praveen <37235355+praveenhp07@users.noreply.github.com>
2019-02-05 14:53:56 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,10 @@
+st1, st2 = raw_input().split()
+a=int(st1)
+b=int(st2)
+for num in range(a,b + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
