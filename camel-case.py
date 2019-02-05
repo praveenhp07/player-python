3e0a60771026e7e1cc963a6f03d2efdc42f2f1ee
Praveen <37235355+praveenhp07@users.noreply.github.com>
2019-02-05 14:29:42 +0530

    Create camel-case.py

---
+++ camel-case.py
@@ -0,0 +1,12 @@
+try:
+	s1=raw_input()
+	a=s1.split(' ')
+	g=" "
+	for i in range(0,len(a)):
+			a[i]=a[i].capitalize()
+	c=a[0]
+	for i in range(1,len(a)):
+		c=c+g+a[i]
+	print c
+except:
+ print "Invalid"
